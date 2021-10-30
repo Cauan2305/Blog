6b2fb5d21e66e0747adc6334e132524c3f679e14
cauan matheus <cauanmatheustrigo68@gmail.com>
2021-10-30 14:32:30 -0300

    commit

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'blog_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
